f5d837e76e435fb424904ebeb193afe0eff51ca0
吴占超 <zhanchao.wu@icloud.com>
2019-07-05 13:40:28 +0800

    增加d.ts

---
+++ @types/midway-joi-swagger2.d.ts
@@ -0,0 +1,25 @@
+import { Application } from 'midway';
+import {
+  WrapperOptions,
+  IClassIn,
+  IMethodIn,
+  ISwaggerConfig
+} from '../lib/interface';
+
+/*
+ * @Author: 吴占超
+ * @Date: 2019-07-05 09:39:05
+ * @Last Modified by: 吴占超
+ * @Last Modified time: 2019-07-05 13:40:01
+ */
+export declare function wrapper(
+  app: Application,
+  options?: WrapperOptions
+): void;
+
+export declare function SwaggerJoiController(paramIn: IClassIn): ClassDecorator;
+export declare function SwaggerJoiGet(paramIn: IMethodIn): MethodDecorator;
+export declare function SwaggerJoiPost(paramIn: IMethodIn): MethodDecorator;
+export declare function SwaggerJoiPut(paramIn: IMethodIn): MethodDecorator;
+export declare function SwaggerJoiDel(paramIn: IMethodIn): MethodDecorator;
+export declare const ISwaggerConfig: ISwaggerConfig;
